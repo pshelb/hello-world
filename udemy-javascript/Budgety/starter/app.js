030e07a260810fabfe1306b46ca9bef92be5d9ef
pshelb <pshelberg@gmail.com>
2018-10-15 14:32:58 +1000

    Read input data

--- udemy-javascript/Budgety/starter/app.js
+++ udemy-javascript/Budgety/starter/app.js
@@ -8,16 +8,39 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
-    // some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputBtn: '.add__btn'
+    };
+
+    return {
+        getinput: function () {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value
+            };
+        },
+
+        getDOMstrings: function () {
+            return DOMstrings;
+        }
+    }
 
 })();
 
 // GLOBAL APP CONTROLLER
 var controller = (function (budgetCtrl, UICtrl) {
 
+    var DOM = UICtrl.getDOMstrings();
+    
     var ctrlAddItem = function () {
 
         // 1. Get the field input data
+        var input = UICtrl.getinput();
+        console.log(input);
 
         // 2. Add the item to the budget controller
 
@@ -27,10 +50,9 @@ var controller = (function (budgetCtrl, UICtrl) {
 
         // 5. Display the budget on the UI
 
-        console.log('It works');
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function (event) {
 
